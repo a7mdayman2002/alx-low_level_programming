3718989d23969dee8914d71bf1428269dad4e8d1
a7mdayman2002@gmail.com <a7mdayman2002@gmail.com>
2023-09-14 16:16:09 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+/**
+ *  Author: Masterpiece513
+ *  Description: header file containing function declarations
+ *  void prototypes (void)
+ *  int prototypes (int)
+ */
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
