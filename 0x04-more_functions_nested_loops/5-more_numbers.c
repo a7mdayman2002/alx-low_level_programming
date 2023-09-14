030ba59452541dd7d05598df2586b0365b0ff2f3
a7mdayman2002@gmail.com <a7mdayman2002@gmail.com>
2023-09-14 16:25:12 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers 0-14, 10 times
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
