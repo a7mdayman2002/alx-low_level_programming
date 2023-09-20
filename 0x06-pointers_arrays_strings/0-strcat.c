2cf4325f6696216978734bf454ab14b9ad3aa282
a7mdayman2002@gmail.com <a7mdayman2002@gmail.com>
2023-09-20 01:12:47 -0700

    main

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concat arrays
+ * @dest: array to cocat to
+ * @src: to be concated
+ * Return: new dest array
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
